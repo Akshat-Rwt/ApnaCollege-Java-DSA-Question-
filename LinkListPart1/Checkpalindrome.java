dde8a91fc67f991a2d3646a61ed4292261007059
Akshat Rawat <111777625+Akshat-Rwt@users.noreply.github.com>
2023-06-29 17:16:54 +0530

    Add files via upload

---
+++ LinkListPart1/Checkpalindrome.java
@@ -0,0 +1,232 @@
+package LinkListPart1;
+
+public class Checkpalindrome {
+     public static class Node {
+
+        int data ;
+        Node next ;
+        
+
+        public Node (int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head ;
+    public static Node tail ;
+    public static int  size;
+
+    public void addFirst(int data){
+
+        //Step1 - Create new node 
+        Node newNode = new Node(data);
+        size ++;
+
+        if(head == null){
+            head = tail = newNode ;
+            return ;
+        }
+
+        //Step 2 - newNode  next = head
+        newNode.next = head ; //Link
+
+
+        //Step 3 - head = newNode 
+        head = newNode ;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size ++;
+
+        if(head == null){
+            head = tail = newNode ;
+            return ;
+
+        }
+
+        tail.next = newNode ;
+        tail = newNode ;
+    }
+
+    public void print(){
+        Node  temp = head ; 
+
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next ;
+        }
+
+        System.out.println("null");
+    }
+
+    public void add(int idx , int data){
+
+        if(idx == 0){
+            addFirst(data);
+            return ; 
+        }
+
+        Node newNode = new Node(data);
+        size ++;
+
+        Node temp = head ;
+        int i = 0 ;
+        while(i < idx -1){
+            temp = temp.next ;
+            i++;
+        }
+
+        //i = idx ; temp -> previous node
+
+        newNode.next = temp.next ;
+        temp.next = newNode ;
+    }
+
+    public int removeFirst(){
+
+        if(size == 0 ){
+            System.out.print("Linked lISt is Invalid");
+            return Integer.MIN_VALUE ;
+        }
+
+        else if(size == 1){
+            int val = head.data ;
+            head = tail = null ;
+            return val ;
+
+        }
+        int val = head.data ;
+        head = head.next;
+
+        size-- ;
+
+        return val ;
+    }
+
+    public int removeLast(){
+
+        if(size == 0){
+            System.out.print("LL is empty");
+            return Integer.MIN_VALUE ;
+
+        }
+
+        else if (size ==  1){
+            int val  = head.data ;
+            head  = tail = null ;
+            size  = 0 ;
+            return val  ;
+
+        }
+
+        //prev i = size -2 
+
+        Node prev = head ;
+        for(int i =0 ; i < size-2 ;  i ++){
+            prev = prev.next ;
+        }
+
+
+        int val  = prev.next.data ; //Tail data 
+        prev.next = null ;
+        tail = prev ;
+        size -- ;
+        return val ;
+
+    }
+
+    public Node FindMid(Node head ){
+        Node slow = head ;
+        Node fast = head ;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next ; //+1
+            fast = fast.next.next; //+2
+
+        }
+
+        return slow;  //SLOW IS my node ;
+    }
+
+    public boolean CheckPalindrome(){
+        if(head == null || head.next == null ){
+            return true ;
+        }
+
+        //Step-1 Find Mid 
+
+        Node midNode = FindMid(head);
+
+        //Step-2 Reverse 2ND Half 
+
+        Node prev = null ;
+        Node curr =midNode ;
+        Node next ;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev ;
+            prev = curr;
+            curr =next ;
+
+        }
+
+        Node right = prev ; //Right Half Head
+        Node left = head;
+
+        //Step3 Check Left half 
+        while (right != null){
+            if(left.data != right.data){
+                return false ;
+
+            }
+            left = left.next;
+            right = right.next;
+
+        }
+        return true ;
+
+    }
+
+    
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        //ll.print();
+        ll.addFirst(1);
+
+        //ll.print();
+        ll.addFirst(2);
+
+        //ll.print();
+        ll.addLast(1);
+
+        //ll.print();
+        ll.addLast(2);
+
+        // ll.add(2, 3);
+
+        ll.print();
+      
+        // System.out.println();
+
+        // ll.removeFirst();
+        // ll.print();
+
+        // System.out.println(size);
+
+        // System.out.println();
+
+        // ll.removeLast();
+        // ll.print();
+        // System.out.println(ll.size);
+      
+        System.out.println(ll.CheckPalindrome());
+     
+    }
+
+   
+
+}
---
+++ LinkListPart1/DeleteNthPostionNumber.java
@@ -0,0 +1,206 @@
+package LinkListPart1;
+
+public class DeleteNthPostionNumber {
+     public static class Node {
+
+        int data ;
+        Node next ;
+        
+
+        public Node (int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head ;
+    public static Node tail ;
+    public static int  size;
+
+    public void addFirst(int data){
+
+        //Step1 - Create new node 
+        Node newNode = new Node(data);
+        size ++;
+
+        if(head == null){
+            head = tail = newNode ;
+            return ;
+        }
+
+        //Step 2 - newNode  next = head
+        newNode.next = head ; //Link
+
+
+        //Step 3 - head = newNode 
+        head = newNode ;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size ++;
+
+        if(head == null){
+            head = tail = newNode ;
+            return ;
+
+        }
+
+        tail.next = newNode ;
+        tail = newNode ;
+    }
+
+    public void print(){
+        Node  temp = head ; 
+
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next ;
+        }
+
+        System.out.println("null");
+    }
+
+    public void add(int idx , int data){
+
+        if(idx == 0){
+            addFirst(data);
+            return ; 
+        }
+
+        Node newNode = new Node(data);
+        size ++;
+
+        Node temp = head ;
+        int i = 0 ;
+        while(i < idx -1){
+            temp = temp.next ;
+            i++;
+        }
+
+        //i = idx ; temp -> previous node
+
+        newNode.next = temp.next ;
+        temp.next = newNode ;
+    }
+
+    public int removeFirst(){
+
+        if(size == 0 ){
+            System.out.print("Linked lISt is Invalid");
+            return Integer.MIN_VALUE ;
+        }
+
+        else if(size == 1){
+            int val = head.data ;
+            head = tail = null ;
+            return val ;
+
+        }
+        int val = head.data ;
+        head = head.next;
+
+        size-- ;
+
+        return val ;
+    }
+
+    public int removeLast(){
+
+        if(size == 0){
+            System.out.print("LL is empty");
+            return Integer.MIN_VALUE ;
+
+        }
+
+        else if (size ==  1){
+            int val  = head.data ;
+            head  = tail = null ;
+            size  = 0 ;
+            return val  ;
+
+        }
+
+        //prev i = size -2 
+
+        Node prev = head ;
+        for(int i =0 ; i < size-2 ;  i ++){
+            prev = prev.next ;
+        }
+
+
+        int val  = prev.next.data ; //Tail data 
+        prev.next = null ;
+        tail = prev ;
+        size -- ;
+        return val ;
+
+    }
+
+    
+
+    public void deleteNthFromEnd(int n ){
+
+        //Calculate 
+        int sz = 0;
+        Node temp = head ;
+        
+        while (temp != null){
+            temp = temp.next ;
+            sz++;
+
+        }
+
+        if(n == sz){
+            head = head.next;
+            return  ;
+        }
+
+        int i = 1 ;
+        int iToFind = sz-n ;
+        Node prev  = head ;
+        while(i < iToFind){
+            prev = prev.next ;
+            i++ ;
+        }
+
+        prev.next = prev.next.next ;
+        return ;
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        //ll.print();
+        ll.addFirst(2);
+
+        //ll.print();
+        ll.addFirst(1);
+
+        //ll.print();
+        ll.addLast(4);
+
+        //ll.print();
+        ll.addLast(5);
+
+        ll.add(2, 3);
+
+        ll.print();
+      
+        // System.out.println();
+
+        // ll.removeFirst();
+        // ll.print();
+
+        // System.out.println(size);
+
+        // System.out.println();
+
+        // ll.removeLast();
+        // ll.print();
+        // System.out.println(ll.size);
+      
+      ll.deleteNthFromEnd(3);
+      ll.print();
+    }
+}
